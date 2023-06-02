a31eb48acd639a59bfd0a946c057bff58b60ebac
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-06-02 22:34:35 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Costs from "./Components/Costs";
+
+function App() {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: "Холодильник",
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: "MacBook",
+      amount: 1254.72,
+    },
+    {
+      date: new Date(2021, 3, 1),
+      description: "Jeanse",
+      amount: 49.99,
+    },
+  ];
+  return (
+    <div>
+      <h1>Розпочнемо навчання з React</h1>
+      <Costs costs={costs} />
+    </div>
+  );
+}
+
+export default App;
